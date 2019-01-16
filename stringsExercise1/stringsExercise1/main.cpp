957abf1e36e524779f09929e90e32bf47e8d64b6
bwuphan <bwuphan@gmail.com>
2019-01-15 22:20:39 -0800

    Finish stringsExercise1

--- stringsExercise1/stringsExercise1/main.cpp
+++ stringsExercise1/stringsExercise1/main.cpp
@@ -7,9 +7,24 @@
 //
 
 #include <iostream>
+#include <string>
 
+using namespace std;
 int main(int argc, const char * argv[]) {
-  // insert code here...
-  std::cout << "Hello, World!\n";
+  string unformatted_full_name {"StephenHawking"};
+  
+  //----DO NOT MODIFY THE CODE ABOVE THIS LINE----
+  //----WRITE YOUR CODE BELOW THIS LINE----
+  string first_name (unformatted_full_name, 0, 7);
+  string last_name = unformatted_full_name.substr(7);
+  cout << first_name << endl;
+  cout << last_name << endl;
+  
+  string formatted_full_name = first_name + last_name;
+  formatted_full_name.insert(7, " ");
+  
+  //----WRITE YOUR CODE ABOVE THIS LINE----
+  //----DO NOT MODIFY THE CODE BELOW THIS LINE----
+  cout << formatted_full_name;
   return 0;
 }
