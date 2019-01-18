a093d7114da23301752bb877364889dab684bc61
bwuphan <bwuphan@gmail.com>
2019-01-17 22:54:14 -0800

    Finish basic requirements section 10 challenge

--- section10_challenge/section10_challenge/main.cpp
+++ section10_challenge/section10_challenge/main.cpp
@@ -7,8 +7,36 @@
 //
 
 #include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string coder (string phrase, int number, bool decode) {
+  vector <int> asciiVec {};
+  string encodedString = "";
+  for (size_t i = 0; i < phrase.length(); ++i) {
+    int asciiNum {};
+    if (decode == true) {
+      asciiNum = phrase[i] + number;
+    } else {
+      asciiNum = phrase[i] - number;
+    }
+    char asciiChar = asciiNum;
+    encodedString += asciiChar;
+  }
+  
+  return encodedString;
+}
+
 
 int main(int argc, const char * argv[]) {
-  std::cout << "Hello, World!\n";
+  string input {};
+  cout << "Please input a string to be encoded: " << endl;
+  getline(cin, input);
+  
+  string encoded = coder(input, 2, true);
+  string decoded = coder(encoded, 2, false);
+  cout << encoded << endl << decoded;
   return 0;
 }
