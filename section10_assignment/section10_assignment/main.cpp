c353f8a8562f5f8d665dae251659ac4011e9ad41
bwuphan <bwuphan@gmail.com>
2019-02-24 15:14:30 -0800

    Add section 10 assignment

---
+++ section10_assignment/section10_assignment/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  section10_assignment
+//
+//  Created by Bobby on 2/24/19.
+//  Copyright © 2019 Bobby. All rights reserved.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+void printPyramid(string word) {
+  
+}
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
