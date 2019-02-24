80fee3090869b5834d8dc63e70749e78588173d7
bwuphan <bwuphan@gmail.com>
2019-02-24 15:25:42 -0800

    Add printPyramid function to section10_assignment

--- section10_assignment/section10_assignment/main.cpp
+++ section10_assignment/section10_assignment/main.cpp
@@ -10,15 +10,24 @@
 #include <string>
 #include <vector>
 
-
 using namespace std;
 
-void printPyramid(string word) {
-  
+void printPyramid(string word, int index) {
+  string row {};
+  if (index == word.length()) {
+    return;
+  }
+  int numSpaces = word.length() - index + 1;
+  for (int i = numSpaces; i <= 0; i--) {
+    row += " ";
+  }
 }
 
 int main(int argc, const char * argv[]) {
   // insert code here...
-  std::cout << "Hello, World!\n";
+  string inputWord {};
+  cout << "Enter a word." << endl;
+  getline(cin, inputWord);
+  cout << inputWord;
   return 0;
 }
