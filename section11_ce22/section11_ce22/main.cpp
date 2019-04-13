1aa11bb6ae8dbe2ccc9411a0456db5cd61567abf
bwuphan <bwuphan@gmail.com>
2019-04-13 15:03:18 -0700

    Add section 11 coding exercise 22

---
+++ section11_ce22/section11_ce22/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  section11_ce22
+//
+//  Created by Bobby on 4/13/19.
+//  Copyright © 2019 Bobby. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  std::cout << "Hello, World!\n";
+  return 0;
+}
