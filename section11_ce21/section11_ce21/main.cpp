933a160df86b247fbb727df3bbb60cfe742568c2
bwuphan <bwuphan@gmail.com>
2019-04-13 14:06:16 -0700

    Finish solution for section 11 coding exercise 21

--- section11_ce21/section11_ce21/main.cpp
+++ section11_ce21/section11_ce21/main.cpp
@@ -10,18 +10,20 @@
 #include <cmath>
 using namespace std;
 
-int main() {
+void c_math_functions() {
   
   double bill_total {102.78};
   int number_of_guests {5};
   
   //DO NOT MODIFY THE CODE ABOVE THIS LINE----
   //----WRITE YOUR CODE BELOW THIS LINE----
-  double individual_bill_1 = ceil(bill_total / number_of_guests);
-  cout << individual_bill_1;
+  double individual_bill = bill_total / number_of_guests;
+  double individual_bill_1 = floor(individual_bill);
+  double individual_bill_2 = round(individual_bill);
+  double individual_bill_3 = ceil(individual_bill * 100) / 100;
+  
   
   //----WRITE YOUR CODE ABOVE THIS LINE----
   //----DO NOT MODIFY THE CODE BELOW THIS LINE----
-//  cout << "The individual bill at location 1 will be $" << individual_bill_1 << "\n" << "The individual bill at location 2 will be $" << individual_bill_2 << "\n" << "The individual bill at location 3 will be $" << individual_bill_3;
-  return 0;
+  cout << "The individual bill at location 1 will be $" << individual_bill_1 << "\n" << "The individual bill at location 2 will be $" << individual_bill_2 << "\n" << "The individual bill at location 3 will be $" << individual_bill_3;
 }
