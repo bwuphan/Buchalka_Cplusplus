382a52e25a24e455f6b304c0dc297cb31f67e2d6
bwuphan <bwuphan@gmail.com>
2019-06-02 18:09:18 -0700

    Add print to section13_challenge

--- section13_challenge/section13_challenge/main.cpp
+++ section13_challenge/section13_challenge/main.cpp
@@ -8,8 +8,29 @@
 
 #include <iostream>
 
+
+using namespace std;
+
+void print(int *array, size_t size) {
+  for (size_t i = 0; i < size; ++i) {
+    cout << array[i] << " ";
+  }
+}
+
 int main(int argc, const char * argv[]) {
   // insert code here...
-  std::cout << "Hello, World!\n";
+  const size_t array1_size{5};
+  const size_t array2_size{3};
+  
+  int array1[] {1,2,3,4,5};
+  int array2[] {10,20,30};
+  
+  cout << "Array 1: ";
+  print (array1, array1_size);
+  
+  cout << "Array 2: ";
+  print(array2, array2_size);
+  
+  
   return 0;
 }
