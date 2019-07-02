b6758ac174318246aeee2adfb21b6bf0ac63a27a
bwuphan <bwuphan@gmail.com>
2019-07-01 22:43:10 -0700

    Add solution for exercise29

--- section13/exercise29/Dog.h
+++ section13/exercise29/Dog.h
@@ -5,12 +5,27 @@ using namespace std;
 
 class Dog {
 private:
-    string name;
-    int age;
+  string name;
+  int age;
 public:
 
 //---- WRITE YOUR CLASS FUNCTIONS BELOW THIS LINE----
-
+  string get_name() {
+    return name;
+  }
+  
+  string set_name(string newName) {
+    name = newName;
+  }
+  
+  int get_age() {
+    return age;
+  }
+  
+  int set_age(int newAge) {
+    age = newAge;
+  }
+  
 
 //---- WRITE YOUR CLASS FUNCTIONS ABOVE THIS LINE----
 };
